072382ec1807a50103dcb27b8beaef65d6da55dd
aynakeya <aynakeya.official@gmail.com>
2024-04-23 22:48:57 -0700

    add lyric interface

--- lyric/lyric.go
+++ lyric/lyric.go
@@ -26,6 +26,7 @@ type LyricStorageRepository interface {
 	scene.Repository
 	GetLyric(title string, artist string) (result []Song, err error)
 	Add(song []Song) (err error)
+	//Search(keyword string) (result []Song, err error)
 }
 
 type LyricProvider interface {
